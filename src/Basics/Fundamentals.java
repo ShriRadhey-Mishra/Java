98fc821b25e74a6b9e4a4d55fd25848d3161839f
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-07-11 23:40:13 +0530

    started with fundamentals

---
+++ src/Basics/Fundamentals.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class Fundamentals {
+    public static void main(String[] args) {
+        int num = 9;
+        double d = 9.8;
+        float f = 8.5f;
+        char c = 'a';
+
+        System.out.println("Hello, World!");
+        System.out.print("I am ");
+        show();
+    }
+
+    public static void show() {
+        System.out.println("in show!");
+    }
+}
